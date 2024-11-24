7dbb49b81ab13f6e3ad8f364aa1543da7a68dbfc
Vivek Ramesh <coinhexa@gmail.com>
2024-11-24 21:43:53 +0530

    fix: make csrf tokens and cors work

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,6 @@
-export default ({ $axios, store }) => {
+import SetCookieParser from 'set-cookie-parser'
+
+export default ({ $axios, $cookies, store }) => {
   // Inject $hello(msg) in Vue, context and store.
   $axios.defaults.timeout = 30000
 
@@ -14,5 +16,14 @@ export default ({ $axios, store }) => {
       config.headers['X-CSRF-Token'] = csrfToken
     }
     return config
+  })
+
+  $axios.onResponse((response) => {
+    const cookies = SetCookieParser.parse(response)
+
+    cookies.forEach((cookie) => {
+      const { name, value, ...options } = cookie
+      $cookies.set(name, value, options)
+    })
   })
 }
