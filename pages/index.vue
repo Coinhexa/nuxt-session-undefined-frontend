7e9a36003ddfcab33c0eed8d35d4139dd43af5fe
Vivek Ramesh <coinhexa@gmail.com>
2024-11-24 21:38:48 +0530

    fix: show fetched csrf tokens on the page

--- pages/index.vue
+++ pages/index.vue
@@ -5,13 +5,19 @@
     template(v-else)
       n-link(to='/login') Login
     div Current User is {{$store.state.me.user ? $store.state.me.user : 'still undefined'}} Check the console to see the connect.sid being retrieved but nuxtServerInit inside store/index.js is not able to retrieve req.session or req.user
-    b-button(@click='fetchSessionToken') Get Session Token
+    b-button(@click='fetchSessionToken') Fetch Session Token
+    p {{ csrfToken ? `Yay! here is our token: ${csrfToken}` : 'Haven\'t fetched any CSRF tokens yet 😫' }}
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
+  data() {
+    return {
+      csrfToken: null,
+    }
+  },
   computed: {
     ...mapGetters({ user: 'me/GET_USER' }),
   },
@@ -21,7 +27,8 @@ export default {
     },
     async fetchSessionToken() {
       const { data } = await this.$axios.get('/session/token')
-      console.log('FETCH SESSION TOKEN', data);
+      console.log('FETCH SESSION TOKEN', data.token)
+      this.csrfToken = data.token
     },
   },
 }
@@ -30,5 +37,9 @@ export default {
 <style lang="scss" scoped>
 .container {
   height: 100%;
+}
+p {
+  max-width: 100%;
+  word-wrap: break-word;
 }
 </style>
